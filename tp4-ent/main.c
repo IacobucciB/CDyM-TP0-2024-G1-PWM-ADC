40320eb61424a982678840109af9f5e5c93844d8
Iacobucci Juan B. <link00222@gmail.com>
2024-07-04 18:43:28 -0300

    atmel project created

---
+++ tp4-ent/main.c
@@ -0,0 +1,18 @@
+/*
+ * tp4-ent.c
+ *
+ * Created: 4/7/2024 18:41:45
+ * Author : Bauti
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
