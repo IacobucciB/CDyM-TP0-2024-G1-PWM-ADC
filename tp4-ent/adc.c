f0dbed1dde17de5a2f300f72d3efff00cc383b38
IacobucciB <link00222@gmail.com>
2024-07-08 10:18:01 -0300

    adc init, not working

--- tp4-ent/adc.c
+++ tp4-ent/adc.c
@@ -8,4 +8,21 @@ void ADC_init() {
 	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
+uint16_t ADC_read(uint8_t channel) {
+	// Select ADC Channel. Channel must be 0-7. Channel will always be ADC3.
+	ADMUX &= 0xF0;
+	ADMUX |= channel;
 
+	// Start Single conversion
+	ADCSRA |= (1 << ADSC);
+	
+	// Wait for conversion to complete
+	while((ADCSRA&(1<<ADIF))==0);
+
+	// Clear the flag
+	ADCSRA |= (1<<ADIF);
+	
+	return ADC;
+	
+	
+}
--- tp4-ent/main.c
+++ tp4-ent/main.c
@@ -4,8 +4,14 @@
 #include <avr/interrupt.h>
 
 #include "rgb.h"
+#include "adc.h"
 
+void RGB_setBrightness() {
+	uint16_t adc_value = ADC_read(3); // Read ADC value from ADC3 (PC3)
+	uint8_t brightness = adc_value / 4; // Scale ADC value (0-1023) to PWM value (0-255)
 
+	RGB_setColor(brightness, brightness, brightness); // Set brightness for all colors
+}
 
 
 int main(void){
@@ -13,12 +19,14 @@ int main(void){
 	RGB_init();
 	sei();
 	
-	RGB_setColor(255,0,255);
+	RGB_setColor(0,0,0);
 
 	
 	
 	while(1){
 		
+		RGB_setBrightness();
+		_delay_ms(5);
 
 
 	}
